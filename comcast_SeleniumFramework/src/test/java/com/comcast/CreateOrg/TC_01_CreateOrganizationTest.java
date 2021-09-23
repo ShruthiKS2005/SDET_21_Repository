4981d4c72395b2416d1068e46af9732ecc4f6114
ShruthiKS2005 <shruthiks2005@gmail.com>
2021-09-23 11:18:29 +0530

    jenkins

---
+++ comcast_SeleniumFramework/src/test/java/com/comcast/CreateOrg/TC_01_CreateOrganizationTest.java
@@ -0,0 +1,22 @@
+package com.comcast.CreateOrg;
+
+import org.testng.annotations.Test;
+
+public class TC_01_CreateOrganizationTest 
+{
+	@Test(groups="smoke")
+	public void createOrganization ()
+	{
+		System.out.println("execute createOrganization");
+	}
+	@Test(groups="regression")
+	public void createOrganizationWithIndustry()
+	{
+		System.out.println("execute createOrganizationWithIndustry");
+	}
+	@Test(groups="regression")
+	public void deleteOrganization()
+	{
+		System.out.println("execute deleteOrganization");
+	}
+}
---
+++ comcast_SeleniumFramework/src/test/java/com/comcast/CreateOrganizationTest/TC_01_CreateContactTest.java
@@ -0,0 +1,22 @@
+package com.comcast.CreateOrganizationTest;
+
+import org.testng.annotations.Test;
+
+public class TC_01_CreateContactTest 
+{
+	@Test(groups="regression")
+	public void createContactTest()
+	{
+		System.out.println("execute createContactTest");
+	}
+	@Test(groups="regression")
+	public void createContactWithOrganizationTest()
+	{
+		System.out.println("execute createContactWithOrganization");
+	}
+	@Test(groups="smoke")
+	public void deleteContactTest()
+	{
+		System.out.println("execute deleteContactTest");
+	}
+}
